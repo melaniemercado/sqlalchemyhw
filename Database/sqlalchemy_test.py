ea2c244a7a88a7ce56158c5d9d711d0a72d3fb1d
melaniemercado <melmercado89@gmail.com>
2019-12-12 14:17:33 -0500

    Fix: fix sqlalchemy test file of syntax error

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -6,6 +6,7 @@ from sqlalchemy import desc, func, cast, Date, distinct, union, DateTime, text, 
 from sqlalchemy import or_, and_, not_
 from datetime import datetime
 from sqlalchemy.exc import IntegrityError
+from pprint import pprint
 
 engine = create_engine('sqlite:////web/sqlite-data/example.db')
 
@@ -181,4 +182,172 @@ o3.order_lines.append(orderline1)
 o3.order_lines.append(orderline2)
 
 session.add_all([o3])
+session.commit()
+
+# Querying Database
+
+# all() method
+session.query(Customer).all()
+print("session.query(Customer).all()=", session.query(Customer).all())
+
+print("session.query(Item).all() session.query(Order).all()=", session.query(Item).all(), session.query(Order).all())
+
+print(session.query(Customer))
+
+q = session.query(Customer)
+
+for c in q:
+    print(c.id, c.first_name)
+
+print("session.query(Customer.id, Customer.first_name).all()=", session.query(Customer.id, Customer.first_name).all())
+session.query(Customer).count() # get the total number of records in the customers table
+session.query(Item).count()  # get the total number of records in the items table
+session.query(Order).count()  # get the total number of records in the orders table
+
+# count() method
+print("session.query(Customer).count()", session.query(Customer).count() ) # get the total number of records in the customers table
+print("session.query(Item).count()", session.query(Item).count())  # get the total number of records in the items table
+print("session.query(Order).count()", session.query(Order).count())  # get the total number of records in the orders table
+
+# get() method
+print("session.query(Customer).get(1)",session.query(Customer).get(1))
+print("session.query(Item).get(1)", session.query(Item).get(1))
+print("session.query(Order).get(100)" , session.query(Order).get(100))
+
+
+#filter() method
+print("session.query(Customer).filter(Customer.first_name == 'John').all()",session.query(Customer).filter(Customer.first_name == 'John').all())
+
+print(session.query(Customer).filter(Customer.first_name == 'John'))
+
+print("session.query(Customer).filter(Customer.id <= 7, Customer.town == 'Norfolk').all()", session.query(Customer).filter(Customer.id <= 7, Customer.town == 'Norfolk').all())
+
+
+print(session.query(Customer).filter(Customer.id <= 7, Customer.town.like("Nor%")))
+
+print("-----------")
+from sqlalchemy import or_, and_, not_
+
+# find all customers who either live in Peterbrugh or Norfolk
+
+print(session.query(Customer).filter(or_(
+    Customer.town == 'Peterbrugh',
+    Customer.town == 'Norfolk'
+)).all())
+
+# find all customers whose first name is John and live in Norfolk
+
+print(session.query(Customer).filter(and_(
+    Customer.first_name == 'John',
+    Customer.town == 'Norfolk'
+)).all())
+
+# Customers with first name John
+output = session.query(Customer).filter(text("first_name = 'John'")).all()
+print("Find all Customers with first name John")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
+
+# Customers with town starting with Nor
+session.query(Customer).filter(text("town like 'Nor%'")).all()
+print("Find all Customers with town starting with Nor")
+for i in output:
+    print("Name: ", i.first_name, " ", i.last_name, " Address:", i.address, " Email:", i.email)
+
+#Dealing with Duplicates
+from sqlalchemy import distinct
+print(session.query(Customer.town).filter(Customer.id  < 10).all())
+print(session.query(Customer.town).filter(Customer.id  < 10).distinct().all())
+
+print(session.query(
+    func.count(distinct(Customer.town)),
+    func.count(Customer.town)
+).all())
+
+#Casting
+from sqlalchemy import cast, Date, distinct, union
+session.query(
+    cast(func.pi(), Integer),
+    cast(func.pi(), Numeric(10, 2)),
+    cast("2010-12-01", DateTime),
+    cast("2010-12-01", Date),
+all()
+
+
+
+# Unions
+s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
+s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
+print(s1.union(s2).all())
+
+
+# Union All
+print(s1.union_all(s2).all())
+
+# Updating Data
+i = session.query(Item).get(8)
+i.selling_price = 25.91
+
+session.add(i)
+session.commit()
+
+# update quantity of all quantity of items to 60 whose name starts with 'W'
+
+print(session.query(Item).filter(
+    Item.name.ilike("W%")
+).update({"quantity": 60}, synchronize_session='fetch'))
+session.commit()
+
+# Deleting data
+i = session.query(Item).filter(Item.name == 'Monitor').one()
+i
+session.delete(i)
+session.commit()
+
+# To delete multiple records at once use the delete() method of the Query object.
+
+session.query(Item).filter(
+    Item.name.ilike("W%")
+).delete(synchronize_session='fetch')
+session.commit()
+
+
+#  Raw Queries
+
+from sqlalchemy import text
+
+print(session.query(Customer).filter(text("first_name = 'John'")).all())
+
+print(session.query(Customer).filter(text("town like 'Nor%'")).all())
+
+print(session.query(Customer).filter(text("town like 'Nor%'")).order_by(text("first_name, id desc")).all())
+
+# Transactions
+def dispatch_order(order_id):
+    # check whether order_id is valid or not
+    order = session.query(Order).get(order_id)
+
+    if not order:
+        raise ValueError("Invalid order id: {}.".format(order_id))
+
+    if order.date_shipped:
+        pprint("Order already shipped.")
+        return
+
+    try:
+        for i in order.order_lines:
+            i.item.quantity = i.item.quantity - i.quantity
+
+        order.date_shipped = datetime.now()
+        session.commit()
+        pprint("Transaction completed.")
+
+    except IntegrityError as e:
+        pprint(e)
+        pprint("Rolling back ...")
+        session.rollback()
+        pprint("Transaction failed.")
+
+
+dispatch_order(1)
 session.commit()
